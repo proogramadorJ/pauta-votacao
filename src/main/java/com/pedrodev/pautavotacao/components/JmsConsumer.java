ee5fc5caa73807aed3d391b76e745a13440a2c36
pedroribeiro <pedro.ribeiro@deliverit.com.br>
2023-02-06 00:06:10 -0300

    Incluido funcionalide de consumo do resultado de votação no topico do RabbitMQ

---
+++ src/main/java/com/pedrodev/pautavotacao/components/JmsConsumer.java
@@ -0,0 +1,24 @@
+package com.pedrodev.pautavotacao.components;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+
+@Component
+public class JmsConsumer {
+
+    @JmsListener(destination = "resultadovotacaoTopic")
+    public void receiveMessageFromTopic(final Message jsonMessage) throws JMSException, JMSException {
+        String messageData = null;
+        System.out.println("Received message in 2nd topic " + jsonMessage);
+        if(jsonMessage instanceof TextMessage) {
+            TextMessage textMessage = (TextMessage)jsonMessage;
+            messageData = textMessage.getText();
+            System.out.println("messageData in 2nd listener:"+messageData);
+        }
+    }
+
+}
